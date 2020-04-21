4d16636a385d262c426f4819c6d659129ce5811e
trunghien123 <44777619+trunghien123@users.noreply.github.com>
2020-04-21 19:10:46 +0700

    application

---
+++ eShopSolution.Application/Catalog/Products/DTO/ProductCreateRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Application.Catalog.Products.DTO
+{
+    public class ProductCreateRequest
+    {
+        public String Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ eShopSolution.Application/Catalog/Products/DTO/ProductEditRequest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Application.Catalog.Products.DTO
+{
+    public class ProductEditRequest
+    {
+    }
+}
